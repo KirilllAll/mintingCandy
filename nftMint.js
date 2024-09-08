e93ba8735f8b31333d1b084d9945a06d101b71fe
Bogdan <shinbogdan@gmail.com>
2024-09-08 22:05:31 +1000

    fix errors

--- nftMint.js
+++ nftMint.js
@@ -3,22 +3,35 @@ import { mplCandyMachine } from "@metaplex-foundation/mpl-candy-machine";
 import {
   transactionBuilder,
   signerIdentity,
+  signerPayer,
   publicKey,
-  generateSigner,
+  generateSigner, sol, createSignerFromKeypair
 } from "@metaplex-foundation/umi";
 import { setComputeUnitLimit } from "@metaplex-foundation/mpl-toolbox";
 import { mintV2 } from "@metaplex-foundation/mpl-candy-machine";
-import { Keypair, PublicKey } from "@solana/web3.js";
 import { TokenStandard } from "@metaplex-foundation/mpl-token-metadata";
 
-const rpcEndpoint = "https://api.mainnet-beta.solana.com";
+const rpcEndpoint = "https://api.devnet.solana.com";
 
-const signer = Keypair.generate(); // Replace with your own keypair if needed
+let signer =
+{
+  secretKey: Uint8Array.from([
+    229, 242, 102, 34, 251, 222, 25, 17, 214, 226, 11,
+    5, 217, 77, 251, 167, 148, 136, 88, 167, 32, 205,
+    213, 183, 35, 67, 34, 48, 235, 94, 8, 29, 152,
+    73, 15, 65, 124, 151, 148, 108, 7, 243, 83, 36,
+    25, 244, 118, 175, 33, 145, 230, 56, 130, 119, 68,
+    58, 98, 152, 41, 83, 68, 11, 53, 122
 
+  ]),
+  publicKey: "BFTathbmoN74NEDnxbKbh8KNs7SahtxkKsDU9Ca69vzm"
+}
+console.log("sig: ", signer.secretKey)
 const umi = createUmi(rpcEndpoint);
-const mySigner = generateSigner(umi);
+const mySigner = createSignerFromKeypair(umi, signer);
 
-umi.use(signerIdentity(mySigner.publicKey)).use(mplCandyMachine());
+
+umi.use(signerPayer(mySigner)).use(signerIdentity(mySigner)).use(mplCandyMachine());
 
 async function getMintTransaction({
   candyMachineAddress,
@@ -47,6 +60,8 @@ async function getMintTransaction({
 
     builder.add(setComputeUnitLimit(umi, { units: 800_000 }));
 
+    console.log("Signer: ", signer, signer.publicKey)
+
     const mint = mintV2(umi, {
       candyMachine: candyMachinePubKey,
       nftMint: nftMintPubKey,
@@ -57,13 +72,17 @@ async function getMintTransaction({
     console.log("Mint tx: ", mint);
     console.log("Mint tx: ", mint.items[0].instruction);
 
-    builder.add(mint);
 
-    console.log("Transaction builder created:", builder);
+    let res = await mint.sendAndConfirm(umi);
+    console.log("Transaction confirmed:", res);
 
+
+    // builder.add(mint);
+    //
+    // const result = await builder.sendAndConfirm(umi);
+    // console.log("Transaction confirmed:", result);
     // try {
-    //   const result = await txBuilder.sendAndConfirm(umi);
-    //   console.log("Transaction confirmed:", result);
+
     // } catch (sendError) {
     //   console.error("Transaction failed:", sendError);
     //   // Additional debugging information
